0ca40449562085acabb30cb1907f394e3321373f
Your Name <your.email@example.com>
2025-11-03 15:54:35 +0000

    Add compute configuration

--- networl-setup.tf
+++ networl-setup.tf
@@ -10,7 +10,6 @@ resource "azurerm_virtual_network" "main" {
 resource "azurerm_subnet" "internal" {
        name                 = "internal"
           resource_group_name  = "cloud-infrastructure-rg"
-             virtual_network_name = azurerm_virtual_network.main.name address_prefixes  = ["10.0.2.0/24"]
-}
-}
+             virtual_network_name = azurerm_virtual_network.main.name 
+             address_prefixes  = ["10.0.2.0/24"]
 }
